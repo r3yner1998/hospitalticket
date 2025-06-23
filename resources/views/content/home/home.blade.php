805b2dacfa5900fa2d7a16ee19078d2ab20d4b04
moob98 <rcabezasporras@gmail.com>
2025-06-23 17:17:07 -0500

    primera carga

---
+++ resources/views/content/home/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts/contentNavbarLayout')
+
+@section('title', '')
+
+@section('content')
+
+
+@endsection
--- resources/views/layouts/sections/navbar/navbar.blade.php
+++ resources/views/layouts/sections/navbar/navbar.blade.php
@@ -35,20 +35,8 @@ $navbarDetached = ($navbarDetached ?? '');
 
       <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
         <!-- Search -->
-        <div class="navbar-nav align-items-center">
-          <div class="nav-item d-flex align-items-center">
-            <i class="bx bx-search bx-md"></i>
-            <input type="text" class="form-control border-0 shadow-none ps-1 ps-sm-2" placeholder="Search..." aria-label="Search...">
-          </div>
-        </div>
         <!-- /Search -->
         <ul class="navbar-nav flex-row align-items-center ms-auto">
-
-          <!-- Place this tag where you want the button to render. -->
-          <li class="nav-item lh-1 me-4">
-            <a class="github-button" href="{{config('variables.repository')}}" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star themeselection/sneat-html-laravel-admin-template-free on GitHub">Star</a>
-          </li>
-
           <!-- User -->
           <li class="nav-item navbar-dropdown dropdown-user dropdown">
             <a class="nav-link dropdown-toggle hide-arrow p-0" href="javascript:void(0);" data-bs-toggle="dropdown">
@@ -83,14 +71,6 @@ $navbarDetached = ($navbarDetached ?? '');
               <li>
                 <a class="dropdown-item" href="javascript:void(0);">
                   <i class="bx bx-cog bx-md me-3"></i><span>Settings</span>
-                </a>
-              </li>
-              <li>
-                <a class="dropdown-item" href="javascript:void(0);">
-                  <span class="d-flex align-items-center align-middle">
-                    <i class="flex-shrink-0 bx bx-credit-card bx-md me-3"></i><span class="flex-grow-1 align-middle">Billing Plan</span>
-                    <span class="flex-shrink-0 badge rounded-pill bg-danger">4</span>
-                  </span>
                 </a>
               </li>
               <li>
